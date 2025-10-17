e164268eccd6467d10ba8e38a42afcb0cea4d17b
geekysmm-hue <geekysmm@gmail.com>
2025-10-17 14:14:07 +0300

    Update BotController.cs

--- Assets/Scripts/Controller/BotController.cs
+++ Assets/Scripts/Controller/BotController.cs
@@ -6,9 +6,9 @@ using Model;
 using Model.Config;
 using UnityEngine;
 using Utilities;
-//hello
-//How are you?
-//i'm fine
+//hello world!
+//я был здесь
+
 namespace Controller
 {
     public class BotController
@@ -74,3 +74,4 @@ namespace Controller
         }
     }
 }
+
